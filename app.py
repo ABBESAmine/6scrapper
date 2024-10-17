b2bd2fb0e16434d28dce75516807afe35a1afe9b
nabil94370 <nabilabbes94370@gmail.com>
2024-10-17 23:34:11 +0200

    render

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--window-size=1920x1080")
+options.binary_location = "/usr/bin/google-chrome"
 
 # Fonction pour initialiser le driver
 def init_driver():
